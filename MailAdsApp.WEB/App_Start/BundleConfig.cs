c87963502d8e79832028bfca272e47a68217bc55
olvitau <olvitau@gmail.com>
2016-06-28 22:07:17 +0400

    add frontend

--- MailAdsApp.WEB/App_Start/BundleConfig.cs
+++ MailAdsApp.WEB/App_Start/BundleConfig.cs
@@ -8,36 +8,32 @@ namespace MailAdsApp.WEB
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Css/site.css"));
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                "~/Content/Css/Bootstrap/bootstrap.css",
+                "~/Content/Css/Bootstrap/bootstrap-theme.css",
+                "~/Content/Css/Bootstrap/bootstrap-additions.css",
+                "~/Content/Css/Bootstrap/angular-ranger.css",
+                "~/Content/Css/Bootstrap/ui-bootstrap-csp.css"
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Scripts/Angular/angular.js",
+                "~/Scripts/Angular/angular-animate.js",
+                "~/Scripts/Angular/angular-touch.js",
+                "~/Scripts/Angular/angular-locale_ru-ru.js",
+                "~/Scripts/Angular/angular-ranger.js"
+            ));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/Bootstrap/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/angular-strap").Include(
+                "~/Scripts/Angular/angular-strap/angular-strap.js",
+                "~/Scripts/Angular/angular-strap/angular-strap.tpl.js",
+                "~/Scripts/Angular/angular-strap/date-parser.js"
+            ));
+            bundles.Add(new ScriptBundle("~/bundles/angular-ui").Include(
+                "~/Scripts/Angular/angular-ui/ui-bootstrap.js",
+                "~/Scripts/Angular/angular-ui/ui-bootstrap-tpls.js"
+            ));
         }
     }
 }
